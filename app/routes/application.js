a8742d4143d5247325328d8df1f1ec7a6318c622
ian knauer <ian.knauer@gmail.com>
2017-01-23 16:19:57 -0800

    removed route declaration

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
-const { Route, inject } = Ember;
+const { inject } = Ember;
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
   flashMessages: inject.service(),
